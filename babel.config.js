4edcf7913c0fcf3638615c1a82bb5b79c5148127
obinote <obinote@gmail.com>
2024-04-09 12:04:50 +0700

    module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+          components: './src/components',
+          screens: './src/screens',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/screens/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Screens = () => {
+  return <View />;
+};
+
+export default Screens;
